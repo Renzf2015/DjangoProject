e80d7f92a1e0c9e4e4940b1d523905a387b8a88c
Renz <328713040@qq.com>
2016-09-24 15:49:32 +0800

    添加rango/about/页面

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse('Rango says hey there world!<br/><a href="/rango/about">About</a>')
+
+def about(request):
+    return HttpResponse('<a href="/rango/">Index</a><br/>Rango says here is the about page!')
