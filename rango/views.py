6faaaaaca31f685ee3614aa91fcd3a05133d7a61
Renz <328713040@qq.com>
2016-09-26 21:03:33 +0800

    显示最多的5个目录

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 # Create your views here.
 
 def index(request):
-    context = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context = {'categories': category_list}
     return render(request, 'rango/index.html', context)
 
 def about(request):
