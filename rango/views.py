619ad28527671daad8dc167e8671bb83557b5ad0
Renz <328713040@qq.com>
2016-09-29 14:41:35 +0800

    添加登陆验证装饰器及退出登录功能

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -117,3 +118,12 @@ def user_login(request):
             return HttpResponse("用户名或密码错误")
     else:
         return render(request, 'rango/login.html', {})
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
