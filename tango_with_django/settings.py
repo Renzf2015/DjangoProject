58481a5bf7ca8bfb53229c5dbb634ab25c0770e9
Renz <328713040@qq.com>
2016-09-30 21:38:30 +0800

    添加及配置drr插件

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration', # add in the registration package
 )
 
 MIDDLEWARE_CLASSES = (
@@ -93,3 +94,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/rango/login/'
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAY = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
